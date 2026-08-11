9189367c76a0cc16141e3fa51e3f517f9a20ba0a
DyelsonMota-fs <Dyelsonm.fs@hotmail.com>
2026-08-11 14:15:57 -0300

    feat: adiciona menu principal do ScreenMatch

--- src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
+++ src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
@@ -1,16 +1,10 @@
 package br.com.alura.ScreenMatch;
 
-import br.com.alura.ScreenMatch.model.DadosEpisodio;
-import br.com.alura.ScreenMatch.model.DadosSerie;
-import br.com.alura.ScreenMatch.model.DadosTemporada;
-import br.com.alura.ScreenMatch.service.ConsumoApi;
-import br.com.alura.ScreenMatch.service.ConverteDados;
+import br.com.alura.ScreenMatch.principal.Principal;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenMatchApplication implements CommandLineRunner {
@@ -21,25 +15,7 @@ public class ScreenMatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&&apikey=7a72508c");
-		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=7a72508c");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 1; i <= dados.totalTemporadas() ; i++) {
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=7a72508c");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
